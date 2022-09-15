5937fabb6e2b8ef3eaf773ebb3ac423b41bbc8ee
rayferric <63957587+rayferric@users.noreply.github.com>
2022-09-15 18:34:54 +0200

    feat: make it work on linux, remove outputconfig modification because it's probably not needeed

--- .volt/cmake/toolchains/LinuxAMD64-LinuxAMD64.cmake
+++ .volt/cmake/toolchains/LinuxAMD64-LinuxAMD64.cmake
@@ -7,3 +7,11 @@ set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+# Force relative RPATH
+set(CMAKE_SKIP_BUILD_RPATH TRUE)
+set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS}    -Wl,-rpath,'$ORIGIN'")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-rpath,'$ORIGIN'")
+# Static libraries are not used
+
+set(CMAKE_NO_SONAME TRUE)
